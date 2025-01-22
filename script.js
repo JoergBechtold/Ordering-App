baa02eb42faebdb1ec324e0990138db9f7020a0f
Jörg Bechtold <joergbechtold@gmx.net>
2025-01-22 20:28:49 +0100

    begin basket section

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function renderAllDishes() {
   let dishesCardDrinksRef = document.getElementById('dishes_cards_drinks');
   let categories = ['burger', 'supplement', 'drinks'];
 
+  showNoValueInBasketScreen(); //check if basket empty or not
+
   emptyTheContainerInnerHtml(dishesCardBurgerRef, dishesCardSupplementRef, dishesCardDrinksRef);
 
   processDishesByCategory(categories, dishesCardBurgerRef, dishesCardSupplementRef, dishesCardDrinksRef);
@@ -28,9 +30,48 @@ function processDishesByCategory(categories, dishesCardBurgerRef, dishesCardSupp
   }
 }
 
-function declareKeysForCategorie(j, categorie, newPrice, idRef) {
-  let nameKey = categorie[j].name;
-  let descriptionKey = categorie[j].description;
+function declareKeysForCategorie(j, category, newPrice, idRef) {
+  let nameKey = category[j].name;
+  let descriptionKey = category[j].description;
 
-  idRef.innerHTML += templateGeneratedDishesCardHtml(newPrice, nameKey, descriptionKey);
+  idRef.innerHTML += templateGeneratedDishesCardHtml(j, newPrice, nameKey, descriptionKey);
+}
+
+//Button Section for Basket
+function switchToDelivery() {
+  let deliveryOption = document.getElementById('delivery');
+  let pickupOption = document.getElementById('pickup');
+
+  if (!deliveryOption.classList.contains('active')) {
+    deliveryOption.classList.add('active');
+    pickupOption.classList.remove('active');
+  }
+}
+
+function switchToPickup() {
+  let deliveryOption = document.getElementById('delivery');
+  let pickupOption = document.getElementById('pickup');
+
+  if (!pickupOption.classList.contains('active')) {
+    pickupOption.classList.add('active');
+    deliveryOption.classList.remove('active');
+  }
+}
+
+//no value in basket
+function showNoValueInBasketScreen() {
+  let noValueInBasketRef = document.getElementById('no_value_in_basket_container');
+
+  if (basket.dish === '') {
+    noValueInBasketRef.style.display = 'flex';
+  }
+}
+
+function addDishToBasket(newPrice, nameKey) {
+  let newPriceForArray = newPrice.replace(',', '.');
+  basket.dish.push(nameKey), basket.price.push(newPriceForArray);
+
+  basket.totalPrice = +basket.totalPrice + +newPriceForArray;
+
+  console.log(basket);
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,13 +1,13 @@
-function templateGeneratedDishesCardHtml(newPrice, nameKey, descriptionKey) {
+function templateGeneratedDishesCardHtml(j, newPrice, nameKey, descriptionKey) {
   return /*html*/ `
-      <div class="single-dishes-card">
+      <div id="single_dishes_card${j}" class="single-dishes-card">
              <h2><b>${nameKey}</b></h2>
              <span><b>${newPrice}€</b></span>
              <span>
               ${descriptionKey}
              </span>
-             <div id="add_dishes_btn" class="add-dishes-btn">
-               <img onclick="addDishToBasket()" src="assets/icons/icon-orange-plus-50.png" alt="Gericht hinzufühen Btn" />
+             <div id="add_dishes_btn${j}" class="add-dishes-btn">
+               <img onclick="addDishToBasket('${newPrice}', '${nameKey}')" src="assets/icons/icon-orange-plus-50.png" alt="Gericht hinzufühen Btn" />
              </div>
            </div>
   `;
